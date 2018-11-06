7c21ceeb8b68db4ec60fa986bf4d76a2ccaaa6c0
Arthur York <yorkar@oregonstate.edu>
2018-11-06 03:14:46 +0000

    finalized gpio pins, correctly changes the frequency of an attached LED

--- analog/analog_adc_2.py
+++ analog/analog_adc_2.py
@@ -1,5 +1,10 @@
 from __future__ import division
+import RPi.GPIO as GPIO
+from time import sleep
 import spidev
+
+BUS_CHANNEL = 0
+DEVICE_CHANNEL = 0
 
 def bitstring(n):
     s = bin(n)[2:]
@@ -7,7 +12,7 @@ def bitstring(n):
 
 def read(adc_channel=0, spi_channel=0):
     conn = spidev.SpiDev(0, spi_channel)
-    conn.open(0, 0)
+    # conn.open(BUS_CHANNEL, DEVICE_CHANNEL)
     conn.max_speed_hz = 1200000 # 1.2 MHz
     cmd = 128
     if adc_channel:
@@ -15,7 +20,22 @@ def read(adc_channel=0, spi_channel=0):
     reply_bytes = conn.xfer([cmd, 0])
     reply_bitstring = ''.join(bitstring(n) for n in reply_bytes)
     reply = reply_bitstring[5:15]
-    return int(reply, 2) / 2 ** 10
+    return int(reply, 2) / 2.0 ** 10.0
 
 if __name__ == '__main__':
     print read()
+    print read()
+
+    led_pin = 16
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(led_pin, GPIO.OUT)
+
+    while 0 != 1:
+        GPIO.output(led_pin, GPIO.HIGH)
+        period = read() * 5
+        sleep(period * 0.5)
+        GPIO.output(led_pin, GPIO.LOW)
+        sleep(period * 0.5)
+
+    GPIO.cleanup()
