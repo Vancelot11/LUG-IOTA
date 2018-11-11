4747f7bdb1378116094203e02b95e0e08fcd04da
Vance <kerryavance@gmail.com>
2018-11-11 23:55:06 +0000

    led added

--- digital/button.py
+++ digital/button.py
@@ -2,12 +2,14 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(13, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 while True:
-    input_state = GPIO.input(23)
-    GPIO.wait_for_edge(23, GPIO.RISING)
+    input_state = GPIO.input(13)
+    time.sleep(0.01)
+    GPIO.wait_for_edge(13, GPIO.RISING)
     if input_state == False:
         print("It works!")
         time.sleep(0.2)
-    
+
+GPIO.cleanup()
