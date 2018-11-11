fbe614dd73384612ca8fb59eb2cf29926cd98c34
Vance <kerryavance@gmail.com>
2018-11-11 23:22:06 +0000

    it works extra well

--- digital/src/button.py
+++ digital/src/button.py
@@ -2,12 +2,13 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(19, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(13, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
-while True:
-    input_state = GPIO.input(19)
-    GPIO.wait_for_edge(19, GPIO.RISING)
-    if input_state == False:                #This depends on pulldown/pullup resistor.
-        print("It works!")
-        time.sleep(0.2)
-    
+try:
+    while True:
+        button_state = GPIO.input(13)
+        if button_state == 1:
+            print('Button Pressed...')
+            time.sleep(0.2)
+except:
+    GPIO.cleanup()
