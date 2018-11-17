e99d828ea7eb3ce9efefd1a3ec87e42712d93029
Monica Holliday <32148936+monicaholliday@users.noreply.github.com>
2018-11-16 17:55:32 -0800

    Update button.py

--- digital/src/button.py
+++ digital/src/button.py
@@ -2,19 +2,19 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(13, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(24, GPIO.OUT)
+GPIO.setup(19, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(26, GPIO.OUT)
 
 
 try:
     while True:
-        button_state = GPIO.input(13)
+        button_state = GPIO.input(19)
         if button_state == 1:
-            GPIO.output(24,1)
+            GPIO.output(26,1)
             print('Button Pressed...')
             time.sleep(0.2)
         else:
-            GPIO.output(24,0)
+            GPIO.output(26,0)
 
 except:
     GPIO.cleanup()
